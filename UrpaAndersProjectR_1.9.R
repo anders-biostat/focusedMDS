32530f337d4f7bb6ce79243b5f06eb06c710f8a8
leamartta <lea.urpa@gmail.com>
2016-09-02 12:54:33 +0300

    Rewritten to create a function focused_mds() with input variables dis (dissimilarity matrix) and focus, a numerical value.

--- UrpaAndersProjectR_1.9.R
+++ UrpaAndersProjectR_1.9.R
@@ -1,12 +1,9 @@
 # Solutions to the mathematical portion of the project in R
 # Lea Urpa, Simon Anders rotation in FIMM-EMBL PhD 
-
-# Load dummy data set
 library(datasets)
-#data("CO2")
-#CO2data <- subset(CO2, conc == 1000)
-#tab <- as.matrix(CO2data$uptake)
-#row.names(tab) <- CO2data$Plant
+library(distnetR)
+
+# Load dummy data set, generate dissimilarity matrix
 
 data("iris")
 tab <- (iris[seq.int(1, nrow(iris), 6), 
@@ -19,99 +16,85 @@ species_ID <- data.frame(species = tab[,"Species"],
 
 tab <- tab[, !names(tab) %in% c("Species")]
 
-# Generate dissimilarity matrix
 dis <- as.matrix(dist(tab))
 
-# Arbitrarily pick a point (let's say 13) and order a the points in a
-# vector with their distance from 13. 
+# The goal here is to create a function that takes inputs m 
+# (dissimilarity matrix) and focus (the point clicked on by 
+# the user), where the output is a data table (r, phi) to plot.
 
-focus <- 13
+focus <- 13    # The point clicked on by the user
 
-obj_by_dist <- names(sort(dis[,focus]))
-
-# Create a data frame with columns ord.vector, r, phi, 
-# where data is ordered by the ordering vector previously created.
-# Set r as the row named as the focus, in the order specified by the
-# ordering vector.
-# Set the first data point phi to NA, and the second data point 
-# phi = pi/2 (arbitrary, looks nice on graph)
-
-plotdata <- data.frame(r = dis[focus, obj_by_dist],
-                       phi = NA,
-                       row.names =obj_by_dist)
-plotdata$phi[1] <- 0
-plotdata$phi[2] <- pi/2
-
-# Here we are defining the new_point, the point which we are selecting
-# the optimal phi for, as the third point in the obj_by_dist vector 
-# (the list of points in order by their distance to the focus point)
-
-new_point <- obj_by_dist[3] 
-
-
-# Write a function that takes phi as an argument and returns stress.
-
-stress <- function(phi){
+focused_mds <- function(m, focus) {
   
-  # position of the new point, given the candidate phi
-  xi <- plotdata[new_point,"r"] * cos(phi)
-  yi <- plotdata[new_point,"r"] * sin(phi)
+  obj_by_dist <- names(sort(dis[,focus]))  # sort points by distance to focus
   
-  stress = 0
-  j = 1                # the iteration variable
+  plotdata <- data.frame(r = dis[focus, obj_by_dist],  # order df by dist to focus
+                         phi = NA,
+                         row.names =obj_by_dist)
   
-  while (j < grep(paste("^",new_point,"$",sep=""), obj_by_dist)) {
-    # position of each of the previous points, iterating 
-    # through the list of previously determined phis
-    xj <- plotdata$r[j] * cos(plotdata$phi[j])
-    yj <- plotdata$r[j] * sin(plotdata$phi[j])
+  plotdata$phi[1] <- 0         # To set focus at origin
+  plotdata$phi[2] <- pi/2      # To set 2nd point on y axis
+  
+  new_point <- obj_by_dist[3]  # We begin optimizing on the third point
+  
+  
+  stress <- function(phi){
     
-    Dij = sqrt( (xi - xj)^2 + (yi - yj)^2)
-    stress = stress + (dis[obj_by_dist[j],new_point] - Dij)^2
-    j = j + 1
+    # position of the new point, given the candidate phi
+    xnew <- plotdata[new_point,"r"] * cos(phi)
+    ynew <- plotdata[new_point,"r"] * sin(phi)
+    
+    stress = 0
+    j = 1                # the iteration variable
+    
+    # position of each of the previous points, iterating through the list of previously determined phis
+    while (j < grep(paste("^",new_point,"$",sep=""), obj_by_dist)) {
+      
+      xj <- plotdata$r[j] * cos(plotdata$phi[j])
+      yj <- plotdata$r[j] * sin(plotdata$phi[j])
+      
+      Dij = sqrt( (xnew - xj)^2 + (ynew - yj)^2)
+      stress = stress + (dis[obj_by_dist[j],new_point] - Dij)^2
+      j = j + 1
+    }
+    return(stress)
   }
-  return(stress)
+  
+  k <- grep(paste("^",new_point,"$",sep=""), obj_by_dist)  # the numerical position of new_point in obj_by_dist
+  
+  while (k <= length(obj_by_dist)) {            # iterate through the list of points and optimize with stress()
+    stress_res <- optimize(stress, c(0,2*pi)) 
+    #print( stress_res )
+    minvalue <- stress_res$minimum
+    plotdata[new_point,"phi"] <- minvalue
+    
+    phigrid <- seq(0, 2*pi, length.out = 100)
+    plot(phigrid,stress(phigrid))
+    abline(v = minvalue)
+    title(main= paste(new_point, "stress graph"))
+    
+    k = k + 1
+    new_point <- obj_by_dist[k]
+  }
+  result <<- plotdata
 }
 
-# pass new_point as a variable, rather than set previously
 
+# Final plot of all values (can be integrated into function once we have final
+# plotting parameters)
 
+species_ID <- (species_ID[match(row.names(result), species_ID$ID),])
 
-# Optimize this function using optimize() and and return the optimal 
-# phi to the appropriate row in the data table.
-# Anchors are necessary, or grep finds multiple IDs there are 
-# characters in common.
-
-j <- grep(paste("^",new_point,"$",sep=""), obj_by_dist)
-
-while (j <= length(obj_by_dist)) {
-  stress_res <- optimize(stress, c(0,2*pi)) 
-  #print( stress_res )
-  minvalue <- stress_res$minimum
-  plotdata[new_point,"phi"] <- minvalue
-  
-  phigrid <- seq(0, 2*pi, length.out = 100)
-  plot(phigrid,stress(phigrid))
-  abline(v = minvalue)
-  title(main= paste(new_point, "stress graph"))
-  
-  j = j + 1
-  new_point <- obj_by_dist[j]
-}
-
-# Final plot of all values
-species_ID <- (species_ID[match(obj_by_dist, species_ID$ID),])
-
-plot((plotdata$r*cos(plotdata$phi)),(plotdata$r*sin(plotdata$phi)), 
+plot((result$r*cos(result$phi)), (result$r*sin(result$phi)), 
      cex=2.5, asp=1, col= species_ID$species )
 grid()
-text((plotdata$r*cos(plotdata$phi)),(plotdata$r*sin(plotdata$phi)), 
-     seq_along(plotdata[,1]) )
+text((result$r*cos(result$phi)), (result$r*sin(result$phi)), 
+     seq_along(rownames(result)) )
 
 # Check the equation for the third point by comparing it to the output of the 
 # stressf function.
 #thirdphicheck <- acos(((dis[2,3]^2 - 2*dis[1,3]^2)/(-2*dis[1,3]))/plotdata[3,2])
 
 distnet( dis+.1, data.frame( 
-  x = plotdata$r*cos(plotdata$phi),
-  y = plotdata$r*sin(plotdata$phi) ) )
+  x = result$r*cos(result$phi),
+  y = result$r*sin(result$phi) ) )
