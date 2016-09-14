be14953965d847c1ccb37c4077fd3cacb8c2e057
leamartta <lea.urpa@gmail.com>
2016-09-14 17:13:02 +0300

    checked the values with the focused_mds function- our home made function has identical answers to the proper optimize() function.

--- optimize_function.R
+++ optimize_function.R
@@ -126,3 +126,84 @@ pdat <- data.frame( x=seq(0, 2*pi, length.out = 100),
 pdat$y <- lapply(pdat$x, f)
 
 plot(pdat$x, pdat$y)
+
+### Run the focused_mds function with this optimization, see if you get the same results 
+focused_mds2 <- function(m, focus) {
+  
+  obj_by_dist <- names(sort(dis[,focus]))  # sort points by distance to focus
+  
+  plotdata <- data.frame(r = dis[focus, obj_by_dist],  # order df by dist to focus
+                         phi = NA,
+                         xcoord = NA,
+                         ycoord = NA,
+                         row.names =obj_by_dist)
+  
+  plotdata <- as.matrix(plotdata)   # handling the plotdata as a matrix is much faster
+  
+  plotdata[1,c("phi","xcoord","ycoord")] <- 0         # To set focus at origin, set phi, x, and y at 0
+  plotdata[2,"phi"] <- pi/2           # To set 2nd point on y axis, designate phi = pi/2
+  plotdata[2,"xcoord"] <- plotdata[2,"r"] * cos(plotdata[2,"phi"]) 
+  plotdata[2,"ycoord"] <- plotdata[2,"r"] * sin(plotdata[2,"phi"])
+  
+  new_point <- obj_by_dist[3]  # We begin optimizing on the third point
+  
+  stress <- function(phi){
+    
+    stress = 0
+    j = 1                # the iteration variable
+    
+    # position of the new point, given the candidate phi
+    xnew <- plotdata[new_point,"r"] * cos(phi)
+    ynew <- plotdata[new_point,"r"] * sin(phi)
+    
+    # position of each of the previous points, iterating through the list of previously determined phis
+    
+    while (j < which(new_point == obj_by_dist)) {   
+      
+      xj <- plotdata[j,"xcoord"]
+      yj <- plotdata[j,"ycoord"]
+      
+      Dij =  sqrt((xnew - xj)^2 + (ynew - yj)^2)
+      stress = stress + (dis[obj_by_dist[j],new_point] - Dij)^2
+      j = j + 1
+    }
+    return(stress)
+  }
+  
+  k <- which(new_point == obj_by_dist)          # the numerical position of new_point in obj_by_dist
+  
+  while (k <= length(obj_by_dist)) {            # iterate through the list of points and optimize with stress()
+    stress_res <- optimize_js(stress, c(0,2*pi))
+    minvalue <- stress_res
+    plotdata[new_point,"phi"] <- minvalue
+    
+    plotdata[new_point, "xcoord"] <- plotdata[new_point,"r"] * cos(plotdata[new_point,"phi"])
+    plotdata[new_point, "ycoord"] <- plotdata[new_point,"r"] * sin(plotdata[new_point,"phi"])
+    
+    #phigrid <- seq(0, 2*pi, length.out = 100)
+    #plot(phigrid,stress(phigrid))
+    #abline(v = minvalue)
+    #title(main= paste(new_point, "stress graph"))
+    
+    k = k + 1
+    new_point <- obj_by_dist[k]
+  }
+  return(as.data.frame(plotdata))
+}
+
+# Set focus to 1st member in dis
+
+focus <- row.names(dis)[1]
+
+# Run focused mds
+
+result2 <- focused_mds(dis,focus)
+
+## Compare results
+par(mfrow=c(1,2))
+plot(result$xcoord, result$ycoord, main="Optimize() Results")
+plot(result2$xcoord, result2$ycoord, main= "optimize_js Results")
+
+result$phi == result$phi
+
+# Yes!!!
