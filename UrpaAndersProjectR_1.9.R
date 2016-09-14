0a9d8c04e653ab9dfd59637c8258e79d649e3d20
leamartta <lea.urpa@gmail.com>
2016-09-14 17:13:56 +0300

    only minor un-commenting changes

--- UrpaAndersProjectR_1.9.R
+++ UrpaAndersProjectR_1.9.R
@@ -2,6 +2,8 @@
 # Lea Urpa, Simon Anders rotation in FIMM-EMBL PhD 
 library(distnetR)
 library(jsonlite)
+library(Biobase)
+library(pheatmap)
 
 ## Interesting data so far:
 ##    - AML patient gene expression data, when patients distances are calculated only with top 10 most variable genes
@@ -23,7 +25,7 @@ library(jsonlite)
     #tab <- readRDS("/Users/admin/Documents/Rotation1_AndersLab/aml.voom.f.rds") 
     #tab <- tab[,!names(tab) %in% c("Entrez")]
     #tab <- t(tab)
-
+    
 ## DSS data
 
 # Correlation coefficients for genes and drugs, calculated over all patients
@@ -37,12 +39,11 @@ library(jsonlite)
     #tab <- readRDS("/Users/admin/Documents/Rotation1_AndersLab/Hs.H.tstat.rds")
     #tab <- tab[[1]]
 
-
 # Getting columns with top 10 variance calculations, generate dissimilarity matrix
 
-tab_var <- apply(tab, 2, var)
-top_10 <- names(tail(sort(tab_var),10))
-tab <- tab[, c(top_10)]
+#tab_var <- apply(tab, 2, var)
+#top_10 <- names(tail(sort(tab_var),10))
+#tab <- tab[, c(top_10)]
 
 
 dis <- as.matrix(dist(tab))
@@ -101,10 +102,10 @@ focused_mds <- function(m, focus) {
     plotdata[new_point, "xcoord"] <- plotdata[new_point,"r"] * cos(plotdata[new_point,"phi"])
     plotdata[new_point, "ycoord"] <- plotdata[new_point,"r"] * sin(plotdata[new_point,"phi"])
     
-    phigrid <- seq(0, 2*pi, length.out = 100)
-    plot(phigrid,stress(phigrid))
-    abline(v = minvalue)
-    title(main= paste(new_point, "stress graph"))
+    #phigrid <- seq(0, 2*pi, length.out = 100)
+    #plot(phigrid,stress(phigrid))
+    #abline(v = minvalue)
+    #title(main= paste(new_point, "stress graph"))
     
     k = k + 1
     new_point <- obj_by_dist[k]
@@ -112,9 +113,9 @@ focused_mds <- function(m, focus) {
   return(as.data.frame(plotdata))
 }
 
-# Set focus to 1
+# Set focus to 1st member in dis
 
-focus <- 1
+focus <- row.names(dis)[1]
 
 # Run focused mds
 
@@ -130,7 +131,7 @@ while (TRUE) {
        #xlim =c(), ylim= c(),
        cex=1, asp=1 )
   #plot(result$xcoord, result$ycoord, cex=1, asp=1)
-  for( r in 1:20 ) lines( r*cos(phig), r*sin(phig), col="lightgray" )
+  for( r in 1:20 ) lines( r*cos(phi), r*sin(phi), col="lightgray" )
   
     
   focus <- identify(result$r*cos(result$phi), result$r*sin(result$phi),
@@ -147,7 +148,46 @@ dis1 <- dis[obj_by_dist,obj_by_dist]
 distnet(dis1, result[,c("xcoord","ycoord")], colors = ifelse(1:52==focus, "red","black"))
 
 ## Output files to javascript format
-toJSON(dis)
+toJSON(row.names(dis))
 
 ## Change the code to output a file in the original order, 
 ## and one in order of the distance of points to the focus (for plotting?)
+
+## Stress function by itself, for optimization timing
+obj_by_dist <- names(sort(dis[,focus])) 
+new_point <- obj_by_dist[3]
+
+stress <- function(phi){
+  
+  stress = 0
+  j = 1                # the iteration variable
+  
+  # position of the new point, given the candidate phi
+  xnew <- plotdata[new_point,"r"] * cos(phi)
+  ynew <- plotdata[new_point,"r"] * sin(phi)
+  
+  # position of each of the previous points, iterating through the list of previously determined phis
+  
+  while (j < which(new_point == obj_by_dist)) {   
+    
+    xj <- plotdata[j,"xcoord"]
+    yj <- plotdata[j,"ycoord"]
+    
+    Dij =  sqrt((xnew - xj)^2 + (ynew - yj)^2)
+    stress = stress + (dis[obj_by_dist[j],new_point] - Dij)^2
+    j = j + 1
+  }
+  return(stress)
+}
+
+system.time(optimize(stress,c(0,2*pi)))
+i =0
+system.time(
+  while (i < 100){
+    optimize(stress,c(0,2*pi))
+    i = i +1
+  }
+  
+)
+
+
