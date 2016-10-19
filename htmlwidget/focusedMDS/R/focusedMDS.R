727b249501fca4297abece842228d36167a23187
leamartta <lea.urpa@gmail.com>
2016-10-19 15:59:53 +0300

    finished R linker, let's see if it works

--- htmlwidget/focusedMDS/R/focusedMDS.R
+++ htmlwidget/focusedMDS/R/focusedMDS.R
@@ -11,9 +11,27 @@ focusedMDS <- function(dis, col_row_names= NULL, color_ids = NULL)  {
 
   # forward options using x
   x = list(
-    message = message
+	  dis = dis, col_row_names= col_row_names, color_ids = color_ids
   )
+  
+  if( !is.matrix(dis) )
+  	stop( "'dis' must be a square matrix or 'dist' object.")
 
+  if( nrow(dis) != ncol(dis))
+  	stop( "Distance matrix 'dis' is not square.")
+
+  if( is.null(col_row_names))
+  	col_row_names <- seq(1, nrow(dis))
+
+  if( nrow(dis) != length(col_row_names))
+  	stop( "Number of rows in 'dis' does not match length of 'col_row_names' array.")
+
+  if( !is.null(color_ids))
+  	color_ids <- rep("black", nrow(dis))
+
+  if( nrow(dis) != nrow(color_ids))
+  	stop( "Number of rows in 'dis' does not match number of rows in 'color_ids' table.")
+  
   # create widget
   htmlwidgets::createWidget(
     name = 'focusedMDS',
@@ -23,28 +41,4 @@ focusedMDS <- function(dis, col_row_names= NULL, color_ids = NULL)  {
     package = 'focusedMDS',
     elementId = elementId
   )
-}
-
-# input the data and save as the object names that js needs
-
-if( !is.matrix(dis) )
-	stop( "'dis' must be a square matrix or 'dist' object.")
-
-if( nrow(dis) != ncol(dis))
-	stop( "Distance matrix 'dis' is not square.")
-
-if( is.null(col_row_names))
-	col_row_names <- seq(1, nrow(dis))
-
-if( nrow(dis) != length(col_row_names))
-	stop( "Number of rows in 'dis' does not match length of 'col_row_names' array.")
-
-if( !is.null(color_ids))
-	color_ids <- rep("black", nrow(dis))
-
-if( nrow(dis) != nrow(color_ids))
-	stop( "Number of rows in 'dis' does not match number of rows in 'color_ids' table.")
-
-htmlwidgets::createWidget ("focused_mds",
-list(dis=dis, col_row_names= col_row_names, color_ids= color_ids))
 }
